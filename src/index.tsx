e1f525efcba3d6b6c7d0d471d259905c4ef21cda
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2019-05-16 00:33:20 -0300

    feat: service worker enabled

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './components/pages/App'
@@ -12,3 +13,8 @@ render(
   </Provider>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
