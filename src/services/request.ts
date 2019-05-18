b6a6d6fe5da40da91fdc6852575f3b67d934ea47
Marcelo rizzardo lima <marcelo_rl2@hotmail.com>
2019-05-17 22:40:00 -0300

    fix: seo fix

--- src/services/request.ts
+++ src/services/request.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const port = process.env.PORT || 3001
 
 const instance = axios.create({
-  baseURL: `http://localhost:${port}/`
+  baseURL: `//localhost:${port}/`
 })
 
 export default instance
